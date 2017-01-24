876927795765b16b263e70c927c95ed275b5154a
SC\sara.nielsen <sara.nielsen@my.simpson.edu>
2017-01-24 09:01:34 -0600

    Edit javascript files

--- web/index.jsp
+++ web/index.jsp
@@ -8,7 +8,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
-    <title>Sara N Project Page</title>
+    <title>Sara Nielsen's Project Page</title>
   </head>
   <body>
   <h1>My Sample Page</h1>
--- web/javascript_assignment.js
+++ web/javascript_assignment.js
@@ -1,7 +1,6 @@
 /**
- * Created by sara.nielsen on 1/19/2017.
+ * Created by Sara on 1/20/2017.
  */
-
 function helloFunction(event) {
     console.log("Hello");
 }
@@ -20,38 +19,38 @@ var button2 = $('#button2');
 button2.on("click", addFunction);
 
 function hideFunction(event) {
-	var hideMe = $('#paragraphToHide');
-	if (hideMe.is(":visible")) {
-		hideMe.hide(500);
-	}
-	else {
-		hideMe.show(500);
-	}
+    var hideMe = $('#paragraphToHide');
+    if (hideMe.is(":visible")) {
+        hideMe.hide(500);
+    }
+    else {
+        hideMe.show(500);
+    }
 }
 
 var button3 = $('#button3');
 button3.on("click", hideFunction);
 
 function validateFunction(event) {
-	var validateMe = $('#phoneField').val();
-	var reg = /^(\d{3})[-](\d{3})[-](\d{4})$/;
-	if(reg.test(validateMe)) {
-		console.log("Ok");
-	}
-	else {
-		console.log("Bad");
-	}
+    var validateMe = $('#phoneField').val();
+    var reg = /^(\d{3})[-](\d{3})[-](\d{4})$/;
+    if(reg.test(validateMe)) {
+        console.log("Ok");
+    }
+    else {
+        console.log("Bad");
+    }
 }
 
 var button4 = $('#button4');
 button4.on("click", validateFunction);
 
 function jsonFunction(event) {
-	var firstName = $('#firstName').val();
-	var lastName = $('#lastName').val();
-	var email = $('#email').val();
-	var formObject = {"fistName" : firstName, "lastName" : lastName, "email" : email};
-	console.log(formObject);
+    var firstName = $('#firstName').val();
+    var lastName = $('#lastName').val();
+    var email = $('#email').val();
+    var formObject = {"fistName" : firstName, "lastName" : lastName, "email" : email};
+    console.log(formObject);
 }
 
 var button5 = $('#button5');
