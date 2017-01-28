8b0a01e7d6585623718830cfebb68615e836d84f
SC\sara.nielsen <sara.nielsen@my.simpson.edu>
2017-01-28 10:22:13 -0600

    Files for assignment 3

---
+++ src/edu/simpson/nielsen/Person.java
@@ -0,0 +1,33 @@
+package edu.simpson.nielsen;
+
+import java.util.Date;
+
+/**
+ * Created by sara.nielsen on 1/26/2017.
+ */
+public class Person {
+    private int id;
+    private String first;
+    private String last;
+    private String email;
+    private String phone;
+    private Date birthday;
+
+    public int getId() {return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getFirst() {return first; }
+    public void setFirst(String first) {this.first = first; }
+
+    public String getLast() {return last; }
+    public void setLast(String last) {this.last = last; }
+
+    public String getEmail() {return email; }
+    public void setEmail(String email) {this.email = email; }
+
+    public String getPhone() {return phone; }
+    public void setPhone(String phone) {this.phone = phone; }
+
+    public Date getBirthday() {return birthday; }
+    public void setBirthday(Date birthday) {this.birthday = birthday; }
+}
